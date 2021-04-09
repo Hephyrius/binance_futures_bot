087b8a28af330a799517366838accc0b122f634e
Hephyrius <20884880+Hephyrius@users.noreply.github.com>
2021-04-09 11:17:04 +0100

    Wrapped in a try catch in case of error -
    Prevent bot from stopping needlessly from minor errors

--- bot.py
+++ bot.py
@@ -27,98 +27,100 @@ in_position = False
 bf.initialise_futures(client, _market=market, _leverage=leverage)
 
 while True:
+    try:
+        #if not currently in a position then execute this set of logic
+        if in_position == False:
+            
+            #generate signal data for the last 500 candles
+            entry = bf.get_signal(client, _market=market, _period=period)
+            
+            #if the second last signal in the generated set of data is -1, then open a SHORT
+            if entry[-2] == -1:
+                print("SELL")
+                bf.initialise_futures(client, _market=market, _leverage=leverage)
+                qty = bf.calculate_position(client, market, _leverage=leverage)
+                bf.execute_order(client, _qty=qty, _side="SELL" , _market=market)
+                market_price = bf.get_market_price(client, _market=market)
+                side = -1
+                in_position = True
+                
+                #close any open trailing stops we have
+                client.cancel_all_orders(market)
+                time.sleep(0.5)
     
-    #if not currently in a position then execute this set of logic
-    if in_position == False:
+                bf.log_trade(_qty=qty, _market=market, _leverage=leverage, _side=side,
+                  _cause="Signal Change", _trigger_price=0, 
+                  _market_price=market_price, _type="Enter")
+                
+                #Let the order execute and then create a trailing stop market order.
+                time.sleep(1)
+                bf.submit_trailing_order(client, _market=market, _qty =qty,
+                                         _callbackRate=trailing_percentage)
+                
+            #if the second last signal in the generated set of data is 1, then open a LONG
+            elif entry[-2] == 1:
+                print("BUY")
+                bf.initialise_futures(client, _market=market, _leverage=leverage)
+                qty = bf.calculate_position(client, market, _leverage=leverage)
+                bf.execute_order(client, _qty=qty, _side="BUY" , _market=market)
+                market_price = bf.get_market_price(client, _market=market)
+                side = 1
+                in_position = True
+                
+                #close any open trailing stops we have
+                client.cancel_all_orders(market)
+                time.sleep(0.5)
+                
+                bf.log_trade(_qty=qty, _market=market, _leverage=leverage, _side=side,
+                  _cause="Signal Change", _trigger_price=0, 
+                  _market_price=market_price, _type="Enter")
+                
+                #Let the order execute and then create a trailing stop market order.
+                time.sleep(1)
+                bf.submit_trailing_order(client, _market=market, _qty =qty,
+                                         _callbackRate=trailing_percentage)
         
-        #generate signal data for the last 500 candles
-        entry = bf.get_signal(client, _market=market, _period=period)
-        
-        #if the second last signal in the generated set of data is -1, then open a SHORT
-        if entry[-2] == -1:
-            print("SELL")
-            bf.initialise_futures(client, _market=market, _leverage=leverage)
-            qty = bf.calculate_position(client, market, _leverage=leverage)
-            bf.execute_order(client, _qty=qty, _side="SELL" , _market=market)
+        #If already in a position then check market and decide when to exit
+        elif in_position == True:
+            
+            #generate signal data for the last 500 candles
+            entry = bf.get_signal(client, _market=market, _period=period)
+            
+            #get the last market price
             market_price = bf.get_market_price(client, _market=market)
-            side = -1
-            in_position = True
             
-            #close any open trailing stops we have
-            client.cancel_all_orders()
-            time.sleep(0.5)
-
-            bf.log_trade(_qty=qty, _market=market, _leverage=leverage, _side=side,
-              _cause="Signal Change", _trigger_price=0, 
-              _market_price=market_price, _type="Enter")
+            #if we generated a signal that is the opposite side of what our position currently is
+            #then sell our position. The bot will open a new position on the opposite side when it loops back around!
+            if entry[-2] != side and entry[-2] != 0:
+                bf.close_position(client, _market=market)
+                
+                #close any open trailing stops we have
+                client.cancel_all_orders(market)
+                time.sleep(0.5)
+                
+                bf.log_trade(_qty=qty, _market=market, _leverage=leverage, _side=side,
+                              _cause="Signal Change", _market_price=market_price, 
+                              _type="EXIT")
+                
+                in_position = False
+                side = 0
+                
+                
+            position_active = bf.check_in_position(client, market)
+            if position_active == False:
+                bf.log_trade(_qty=qty, _market=market, _leverage=leverage, _side=side,
+                  _cause="Signal Change", _market_price=market_price, 
+                  _type="Trailing Stop")
+                in_position = False
+                side = 0
+                
+                #close any open trailing stops we have one
+                client.cancel_all_orders()
+                time.sleep(0.5)
             
-            #Let the order execute and then create a trailing stop market order.
-            time.sleep(1)
-            bf.submit_trailing_order(client, _market=market, _qty =qty,
-                                     _callbackRate=trailing_percentage)
-            
-        #if the second last signal in the generated set of data is 1, then open a LONG
-        elif entry[-2] == 1:
-            print("BUY")
-            bf.initialise_futures(client, _market=market, _leverage=leverage)
-            qty = bf.calculate_position(client, market, _leverage=leverage)
-            bf.execute_order(client, _qty=qty, _side="BUY" , _market=market)
-            market_price = bf.get_market_price(client, _market=market)
-            side = 1
-            in_position = True
-            
-            #close any open trailing stops we have
-            client.cancel_all_orders()
-            time.sleep(0.5)
-            
-            bf.log_trade(_qty=qty, _market=market, _leverage=leverage, _side=side,
-              _cause="Signal Change", _trigger_price=0, 
-              _market_price=market_price, _type="Enter")
-            
-            #Let the order execute and then create a trailing stop market order.
-            time.sleep(1)
-            bf.submit_trailing_order(client, _market=market, _qty =qty,
-                                     _callbackRate=trailing_percentage)
-    
-    #If already in a position then check market and decide when to exit
-    elif in_position == True:
-        
-        #generate signal data for the last 500 candles
-        entry = bf.get_signal(client, _market=market, _period=period)
-        
-        #get the last market price
-        market_price = bf.get_market_price(client, _market=market)
-        
-        #if we generated a signal that is the opposite side of what our position currently is
-        #then sell our position. The bot will open a new position on the opposite side when it loops back around!
-        if entry[-2] != side and entry[-2] != 0:
-            bf.close_position(client, _market=market)
-            
-            #close any open trailing stops we have
-            client.cancel_all_orders()
-            time.sleep(0.5)
-            
-            bf.log_trade(_qty=qty, _market=market, _leverage=leverage, _side=side,
-                          _cause="Signal Change", _market_price=market_price, 
-                          _type="EXIT")
-            
-            in_position = False
-            side = 0
-            
-            
-        position_active = bf.check_in_position(client, market)
-        if position_active == False:
-            bf.log_trade(_qty=qty, _market=market, _leverage=leverage, _side=side,
-              _cause="Signal Change", _market_price=market_price, 
-              _type="Trailing Stop")
-            in_position = False
-            side = 0
-            
-            #close any open trailing stops we have one
-            client.cancel_all_orders()
-            time.sleep(0.5)
-        
-    time.sleep(6)
-    
+        time.sleep(6)
+    except Exception as e:
+        print(f"Encountered Exception {e}")
+        time.sleep(10)
 #%%
 
